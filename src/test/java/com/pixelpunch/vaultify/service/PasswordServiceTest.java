7396e1080973d4c983881bd93aee741a35cc0407
Alexandr022 <112651706+Alexandr022@users.noreply.github.com>
2024-04-14 23:13:32 +0300

    Update PasswordServiceTest.java

--- src/test/java/com/pixelpunch/vaultify/service/PasswordServiceTest.java
+++ src/test/java/com/pixelpunch/vaultify/service/PasswordServiceTest.java
@@ -21,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
 class PasswordServiceTest {
 
     @Mock
