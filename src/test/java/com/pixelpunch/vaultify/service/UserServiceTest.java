5e10b4d8f9215825f2f9784147728f09836f77b0
Alexandr022 <112651706+Alexandr022@users.noreply.github.com>
2024-04-14 23:17:05 +0300

    Update UserServiceTest.java

--- src/test/java/com/pixelpunch/vaultify/service/UserServiceTest.java
+++ src/test/java/com/pixelpunch/vaultify/service/UserServiceTest.java
@@ -24,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
+@SpringBootTest
 class UserServiceTest {
 
     @Mock
