3bb7e7cc062c6f9a9e21e4a258a3dafb5b06bcad
Alexandr022 <112651706+Alexandr022@users.noreply.github.com>
2024-04-15 01:30:31 +0300

    Delete src directory

--- src/main/java/com/pixelpunch/vaultify/core/service/IPasswordGeneratorService.java
+++
@@ -1,5 +0,0 @@
-package com.pixelpunch.vaultify.core.service;
-
-public interface IPasswordGeneratorService {
-    String generatePassword(int length, boolean includeUppercase, boolean includeNumbers, boolean includeSpecial);
-}
