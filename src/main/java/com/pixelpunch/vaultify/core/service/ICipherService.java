0223543d343d12971df96ecae72d014908124708
Alexandr022 <112651706+Alexandr022@users.noreply.github.com>
2024-04-15 10:15:34 +0300

    Update ICipherService.java

--- src/main/java/com/pixelpunch/vaultify/core/service/ICipherService.java
+++ src/main/java/com/pixelpunch/vaultify/core/service/ICipherService.java
@@ -1,17 +1,24 @@
 package com.pixelpunch.vaultify.core.service;
 
+import com.pixelpunch.vaultify.core.exeption.CipherNotFoundException;
 import com.pixelpunch.vaultify.core.model.Cipher;
 import com.pixelpunch.vaultify.web.dto.CipherDto;
 import org.springframework.http.ResponseEntity;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.List;
 
 public interface ICipherService {
     ResponseEntity<Cipher> getCipherById(Long cipherId) throws Exception;
 
-    ResponseEntity<String> createCipher(CipherDto cipherDto, Long userId) throws Exception;
+    ResponseEntity<String> createCipher(CipherDto cipherDto, Long userId) throws CipherNotFoundException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException;
 
-    ResponseEntity<CipherDto> updateCipher(Long cipherId, CipherDto updatedCipherDTO) throws Exception;
+    ResponseEntity<CipherDto> updateCipher(Long cipherId, CipherDto updatedCipherDTO) throws CipherNotFoundException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException;
 
     ResponseEntity<Void> deleteCipher(Long cipherId);
 
