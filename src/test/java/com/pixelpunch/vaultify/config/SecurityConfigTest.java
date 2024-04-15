7ecba5feb21dffe9e1e39a308057a5d28d16c73b
Alexandr022 <112651706+Alexandr022@users.noreply.github.com>
2024-04-15 11:05:46 +0300

    Update SecurityConfigTest.java

--- src/test/java/com/pixelpunch/vaultify/config/SecurityConfigTest.java
+++ src/test/java/com/pixelpunch/vaultify/config/SecurityConfigTest.java
@@ -1,32 +1,48 @@
 package com.pixelpunch.vaultify.config;
 
+import com.pixelpunch.vaultify.web.config.SecurityConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class SecurityConfigTest {
 
-    @Autowired
+    @Mock
     private PasswordEncoder passwordEncoder;
 
-    @Autowired
+    @Mock
     private KeyPairGenerator keyPairGenerator;
+
+    @InjectMocks
+    private SecurityConfig securityConfig;
+
+    @BeforeEach
+    void setUp() throws NoSuchAlgorithmException {
+        MockitoAnnotations.initMocks(this); // Initialize mocks
+        // Mock any specific behavior of your dependencies if needed
+        keyPairGenerator = mock(KeyPairGenerator.class);
+    }
 
     @Test
     void testPasswordEncoderBean() {
-        assertNotNull(passwordEncoder);
+        assertNotNull(securityConfig.passwordEncoder());
     }
 
     @Test
     void testKeyPairGeneratorBean() throws NoSuchAlgorithmException {
-        assertNotNull(keyPairGenerator);
+        assertNotNull(securityConfig.keyPairGenerator());
     }
 }
 
